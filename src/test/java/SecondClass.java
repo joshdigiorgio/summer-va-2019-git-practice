8bfccaace8bca95299dd5fcf7fe92c9f4c2b692b
joshdigiorgio <joshdigiorgio@gmail.com>
2019-11-21 16:46:21 -0500

    another commit

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,6 +1,7 @@
 public class SecondClass {
     public static void main(String[] args) {
-        String myName = "Josh";
+        String myName = "Jimbo";
         System.out.println(myName);
+        System.out.println("shksbj");
     }
 }
