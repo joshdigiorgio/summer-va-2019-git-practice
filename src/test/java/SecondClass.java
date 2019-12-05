91edea13babad6670947b15a554b1c85cc74230a
joshdigiorgio <joshdigiorgio@gmail.com>
2019-12-05 15:52:02 -0500

    changes a line

--- src/test/java/SecondClass.java
+++ src/test/java/SecondClass.java
@@ -1,6 +1,6 @@
 public class SecondClass {
     public static void main(String[] args) {
-        String myName = "Jimbo";
+        String myName = "Samson";
         System.out.println(myName);
         System.out.println("shksbj");
     }
