6dc61eb4ec05627d8469a5d11d85d09bd0851410
joshdigiorgio <55039244+joshdigiorgio@users.noreply.github.com>
2019-12-05 15:28:38 -0500

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,7 +1,7 @@
 public class FirstClass {
 
     public static void main(String[] args) {
-        System.out.println("hello there world");
+        System.out.println("hello there people");
         System.out.println("I am at schooooooool");
     }
 }
